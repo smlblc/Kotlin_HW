66c7c1335224f0f335b3a0cf733de528d02f1d2d
balci <samilbalci.us@gmail.com>
2021-01-21 22:52:07 -0500

    Thursday Homework Task 3.1 / Craeting an Instance of Simple Class

---
+++ src/main/kotlin/Task_3_1_Spice/Main.kt
@@ -0,0 +1,10 @@
+package Task_3_1_Spice
+
+
+    fun main() {
+       val spice = SimpleSpice()
+        print("Spice Name : ${spice.spiceName} , Spiceness is: ${spice.spiceLevel} and SpiceLevel is: ${spice.heat}" )
+
+    }
+
+
---
+++ src/main/kotlin/Task_3_1_Spice/SimpleSpice.kt
@@ -0,0 +1,10 @@
+package Task_3_1_Spice
+
+class SimpleSpice() {
+
+    val spiceName: String = "Curry"
+    var spiceLevel: String = "mild"
+    var heat :Int = 0
+        get() { return 5 }
+
+}
